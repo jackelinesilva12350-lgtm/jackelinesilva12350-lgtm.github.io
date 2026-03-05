ce370a8a90269f6f8dc29557c1d914121f60cf47
jackelinesilva12350-lgtm <jackelinesilva12350@gmail.com>
2026-03-05 10:36:47 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,15 @@
-// Configuração Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBbvROpZivulVavg0az08HQ75kMbNhB4Is",
+  apiKey: "SUA_API",
   authDomain: "sdi-ficha-9d608.firebaseapp.com",
   projectId: "sdi-ficha-9d608",
   storageBucket: "sdi-ficha-9d608.firebasestorage.app",
   messagingSenderId: "1018042015659",
-  appId: "1:1018042015659:web:0f0ec9e7981247ec61fcee",
-  measurementId: "G-8KJ064YJEJ"
+  appId: "1:1018042015659:web:0f0ec9e7981247ec61fcee"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// Inicializar serviços
-const auth = firebase.auth();
-const db = firebase.firestore();
+export const db = getFirestore(app);
